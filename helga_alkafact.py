29b3bb2c0772c0a0fa642e42f4bef5c5e7c4cb9f
Justin Caratzas <bigjust@lambdaphil.es>
2017-07-09 20:47:41 -0400

    add command to add facts, retrieve random based on regex

--- helga_alkafact.py
+++ helga_alkafact.py
@@ -1,5 +1,35 @@
+import smokesignal
+
+from helga import settings
+from helga.db import db
 from helga.plugins import command
 
-@command('alkafact', help='like chuck norris facts, but for alkapwn')
+def add_fact(fact):
+    db.alkafact.insert({
+        'text': fact
+    })
+
+def retrieve_fact(key):
+
+    pipeline = [
+        {'$match': {
+            'text': {'$regex': key}
+        }},
+        {'$sample': {'size': 1}}
+    ]
+
+    result = db.alkafact.aggregate(pipeline)
+
+    try:
+        return result.next()['text']
+    except StopIteration:
+        return 'no results :('
+
+@command('rem', help='like chuck norris facts, but for alkapwn')
 def alkafact(client, channel, nick, message, cmd, args):
-    return 'Success!'
+
+    if args[0] == 'add':
+        add_fact(' '.join(args[1:]))
+        return 'Done!'
+
+    return retrieve_fact(args[0])
